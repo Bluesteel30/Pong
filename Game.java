1507a66b7d75e9e339fbc9b276164fa82a676953
Bluesteel30 <ldionisi@proton.me>
2026-07-13 22:43:05 -0400

    Extending JPanel, and adding import statments to run the graphic side of things

--- Game.java
+++ Game.java
@@ -1,4 +1,8 @@
-public class Game {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.Color;
+public class Game extends JPanel{
 	
 	private int score;
 
@@ -14,6 +18,11 @@ public class Game {
 
 
 	public static void main(String[] args){
-
+		JFrame frame = new JFrame("Pong");
+        Game panel = new Game();
+        frame.setSize(400, 200);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel); // Add the graphics panel to the frame
+        frame.setVisible(true);
 	}
 }
