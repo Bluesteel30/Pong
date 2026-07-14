6d2ed562c054bdb6092952063d78b2a961f28602
Bluesteel30 <ldionisi@proton.me>
2026-07-13 22:18:25 -0400

    Making a new repo. Trying to make a Pong clone

---
+++ Ball.java
@@ -0,0 +1,18 @@
+public class Ball{
+
+private int x;
+private int y;
+private int radius;
+
+public Ball(int x,int y, int r){
+	this.x = x;
+	this.y = y;
+	radius = r;
+}
+
+
+
+
+
+
+}
---
+++ Game.java
@@ -0,0 +1,19 @@
+public class Game {
+	
+	private int score;
+
+	public void increaseScore(){
+		score++;
+	}
+	public void reset(){
+		score = 0;
+	}
+	public int getScore(){
+		return score;
+	}
+
+
+	public static void main(String[] args){
+
+	}
+}
