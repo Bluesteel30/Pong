427878f98a077d491e60237cd988323967a7cf41
Bluesteel30 <ldionisi@proton.me>
2026-07-21 20:44:33 -0400

    Changed the ball speed to be a field of the ball object

--- Ball.java
+++ Ball.java
@@ -3,20 +3,22 @@ public class Ball{
 public int x;
 public int y;
 public int radius;
+public int speed;
 
-public Ball(int x,int y, int r){
+public Ball(int x,int y, int r, int speed){
 	this.x = x;
 	this.y = y;
 	radius = r;
+	this.speed = speed;
 }
 
-public void move(boolean d, int m, int speed){
+public void move(boolean d, int m){
 	if (d){
-	this.x += 1*speed;
-	this.y += m*speed;
+	this.x += 1*this.speed;
+	this.y += m*this.speed;
 	} else {
-	this.x -= 1 * speed;
-	this.y += m * speed;
+	this.x -= 1 * this.speed;
+	this.y += m * this.speed;
 	}
 }
 
